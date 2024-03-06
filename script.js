577621168e52bfaf0211d46990578b02a2cddc16
mjgskyblade <107142589+mjgskyblade@users.noreply.github.com>
2024-03-06 10:03:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -131,3 +131,11 @@ function startRockPaperScissors() {
     // Start the game
     playGame();
 }
+
+// Progress bar 
+window.addEventListener('scroll', () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (scrollTop / scrollHeight) * 100;
+    document.querySelector('.progress-bar').style.width = scrolled + '%';
+});
